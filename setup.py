8b090d3ea038caf670ae45adfbb53ce8709dec90
Arpit Mishra <arpitm1235@gmail.com>
2026-05-21 14:56:50 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-End MLOps Pipeline for Text Classification',
+    author='Arpit Mishra',
+    license='MIT',
+)
